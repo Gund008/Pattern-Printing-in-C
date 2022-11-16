704304ec1e8f3df36e84ee98681e1350c91327b3
Sukanya <106397577+Gund008@users.noreply.github.com>
2022-11-17 00:33:52 +0530

    Add files via upload

---
+++ Pattern8.c
@@ -0,0 +1,52 @@
+/*
+Accept number of rows and number of columns from user and display below pattern,
+
+Input : iRow-4    iCol-4
+
+Output : 1  2  3  4  5
+         1  2        5
+         1     3     5
+         1         4 5
+         1  2  3  4  5
+*/
+
+#include<stdio.h>
+
+void display(int iRow,int iCol)
+{
+    int i=0,j=0;
+
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+             if( (i==1) || (j==iCol) || (i==j) || (j==1) )
+             {
+                printf("%d\t",j);
+             }
+             else
+             {
+                printf(" \t");
+             }
+        }
+        printf("\n");
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter Number of Rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Columns:\n");
+    scanf("%d",&iValue2);
+
+    display(iValue1,iValue2);
+
+    return 0;
+
+}
