d0103c2f7d144e8dea0f155c018ba9ab9b9cf988
Sukanya <106397577+Gund008@users.noreply.github.com>
2022-12-10 15:49:48 +0530

    Add files via upload

--- Pattern21.c
+++ Pattern21.c
@@ -1,53 +1,54 @@
-// Input :  Row 4   Columns     4
 /*
-
-*   *   *   *
-*   *        *
-*       *    *
-*   *   *   *
-
+Input-6, Output-6
+*
+**
+***
+****
+*****
+******
+*****
+****
+***
+**
+*
 */
-
+///////////////////////////////////////////////////////////////////////////
 #include<stdio.h>
 
-void Display(int iRow,int iCol)
+void Display(int iRows,int iCols)
 {
     int i,j=0;
 
-    if(iRow!=iCol)   //filter
+    for(i=1;i<=iRows;i++)
     {
-        return;
-    }
-
-    for(i=0;i<iRow;i++)
-    {
-        for(j=0;j<iCol;j++)
+        for(j=1;j<=i;j++)
         {
-           if((i == 1) || (i == iRow) || (j == 1) || (j == iCol) || (i == j))
-                {
-                    printf("*\t");
-                }
-                else
-                {
-                    printf(" \t");
-                }
+           printf("*");
         }
+        printf("\n");
+    }
+    for(i=iRows-1;i>=1;i--)
+    {
+        for(j=i;j>=1;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
     }
 }
-
+///////////////////////////////////////////////////////////////////////////////
 int main()
 {
-   int iValue1=0;
-   int iValue2=0;
+    int iValue1=0;
+    int iValue2=0;
 
-   printf("Please enter the rows:\n");
-   scanf("%d",&iValue1);
+    printf("Please enter the number of Rows:\n");
+    scanf("%d",&iValue1);
 
-   
-   printf("Please enter the columns:\n");
-   scanf("%d",&iValue2);
+    printf("Please enter the number of Columns:\n");
+    scanf("%d",&iValue2);
 
-   Display(iValue1,iValue2);  //call
-   
+    Display(iValue1,iValue2);
+    
     return 0;
 }
