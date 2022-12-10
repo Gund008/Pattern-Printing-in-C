fddf0945f91ff37b3671eb8ebdb149367f621cb3
Sukanya <106397577+Gund008@users.noreply.github.com>
2022-12-10 15:52:35 +0530

    Add files via upload

---
+++ Pattern22.c
@@ -0,0 +1,54 @@
+/*
+Input-6, Output-6
+*
+**
+***
+****
+*****
+******
+*****
+****
+***
+**
+*
+*/
+///////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void Display(int iRows,int iCols)
+{
+    int i,j=0;
+
+    for(i=1;i<=iRows;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+           printf("*");
+        }
+        printf("\n");
+    }
+    for(i=iRows-1;i>=1;i--)
+    {
+        for(j=i;j>=1;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+
+    printf("Please enter the number of Rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Please enter the number of Columns:\n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+    
+    return 0;
+}
