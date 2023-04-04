45c489bc88dc5ed3b4a1980a54c3bfa96ed1bc72
Sukanya <106397577+Gund008@users.noreply.github.com>
2023-04-04 23:12:09 +0530

    Update Pattern11.c

--- Pattern11.c
+++ Pattern11.c
@@ -15,6 +15,15 @@ Input : 1  2  3  4
 void Pattern(int iRow,int iCol)
 {
     int i=0,j=0;
+        
+    if(iRow < 0)
+    {
+       iRow=-iRow;
+    }
+    if(iCol < 0)
+    {
+       iCol=-iCol;
+    }
         for(i=1;i<=iRow;i++)
         {
           for(j=1;j<=iCol;j++)
